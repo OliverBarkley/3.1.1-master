a7168e1944e0e660c88067564f6644bc3f4f30ad
Oliver Barkley <s0uthsteel@yandex.ru>
2022-03-10 09:44:21 +0300

    Initial commit

---
+++ demoBoot-master/src/main/java/com/example/demoboot/EmployeeService/EmployeeService.java
@@ -0,0 +1,13 @@
+package com.example.demoboot.EmployeeService;
+
+import com.example.demoboot.model.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    void addEmployee(Employee employee);
+    void deleteEmployee(int id);
+    void editEmployee(Employee employee);
+    Employee getEmployeeById(int id);
+    List<Employee> getAllEmployees();
+}
